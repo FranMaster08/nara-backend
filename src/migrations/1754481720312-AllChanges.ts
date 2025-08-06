389b16ae1d5ec8c57b28596dad462e80b4365541
Francisco Javier Jimenez Cohen <francisco.jimenez@fidi.money>
2025-08-06 14:02:14 +0200

    feat(migration): create users table with fields and constraints

---
+++ src/migrations/1754481720312-AllChanges.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class AllChanges1754481720312 implements MigrationInterface {
+    name = 'AllChanges1754481720312'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            CREATE TABLE "users" (
+                "id" SERIAL NOT NULL,
+                "name" character varying(100) NOT NULL,
+                "email" character varying NOT NULL,
+                "age" integer,
+                "role" character varying(50) NOT NULL,
+                "isActive" boolean NOT NULL DEFAULT true,
+                CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"),
+                CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+            DROP TABLE "users"
+        `);
+    }
+
+}
